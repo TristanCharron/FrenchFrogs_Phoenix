b17b2e89a5f1018afb78becbbd9ef9e43637adb3
DESKTOP-L5MT1FV\patlenew <plenouveau@aliceandsmith.com>
2018-08-24 23:50:54 -0400

    Start Environment

---
+++ FrenchFrogsPhoenix/Assets/Scripts/Environment/EnvironmentController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour {
+
+    [SerializeField] EnvironmentObject newObject;
+    public List<EnvironmentObject> currentObjects = new List<EnvironmentObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
