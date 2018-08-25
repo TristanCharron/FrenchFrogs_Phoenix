418bfc71f95a09747ab01464dea9d29564f05121
DESKTOP-L5MT1FV\patlenew <plenouveau@aliceandsmith.com>
2018-08-25 19:04:12 -0400

    Added Utilities Scripts + Added Meteorite Random Direction

---
+++ FrenchFrogsPhoenix/Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    private UIController instance;
+
+    [SerializeField] Text powerTxt;
+    [SerializeField] Text speedTxt;
+    [SerializeField] Text massTxt;
+    [SerializeField] CanvasFader leftUIFader;
+    [SerializeField] CanvasFader RightUIFader;
+
+    // Use this for initialization
+    void Start() {
+        instance = this;
+        DeactivateInGameUI();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void ActivateStartMenuUI()
+    {
+
+    }
+
+    public void DeactivateStartMenuUI()
+    {
+
+    }
+
+    public void ActivateInGameUI()
+    {
+        leftUIFader.GlitchIn();
+        RightUIFader.GlitchIn();
+    }
+
+    public void DeactivateInGameUI()
+    {
+        leftUIFader.Hide();
+        RightUIFader.Hide();
+    }
+
+    public UIController GetInstance()
+    {
+        return instance;
+    }
+
+    public void SetPower(float powerValue)
+    {
+        powerTxt.text = "Power: " + powerValue.ToString();
+    }
+
+    public void SetSpeed(float speedValue)
+    {
+        speedTxt.text = "Speed: " + speedValue.ToString();
+    }
+
+    public void SetMass(float massValue)
+    {
+        massTxt.text = "Mass: " + massValue.ToString();
+    }
+}
---
+++ FrenchFrogsPhoenix/Assets/Scripts/Utils/AutoRotate.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class AutoRotate : MonoBehaviour {
+
+	public float animSpeed = 2f;
+	public bool animOnStart = true, animActivated, animInPlay;
+	private Vector3 rot = Vector3.zero;
+
+	void Start () {
+		if(animOnStart){
+			StartRotation();
+		}
+	}
+
+    public void StartRotation()
+    {
+        animActivated = true;
+    }
+
+    public void StopRotation()
+    {
+        animActivated = false;
+    }
+
+    void Update()
+    {
+        if(animActivated)
+        {
+
+            animInPlay = true;
+            activateAnim();
+        }
+    }
+
+    void activateAnim()
+    {
+        if(animInPlay)
+        {
+            animActivated = false;
+            
+            rot.z = Random.Range(0f, 360f);
+
+			DOTween.Kill(transform);
+            DOTween.To(() => transform.localRotation, trotate => transform.localRotation = trotate, rot,animSpeed).SetEase(Ease.InOutQuad).OnComplete(CurrentAnim);
+			//transform.DOLocalRotate(rot, animSpeed).SetEase(Ease.InOutQuad).OnComplete(CurrentAnim);
+            animInPlay = false;
+        }
+       
+    }
+
+    void CurrentAnim()
+    {
+        animActivated = true;
+    }
+}
