ff2d1b7e857efdf4bbab889c1c447c7494135015
Guillaume Raymond <guiray@users.noreply.github.com>
2018-09-06 01:25:02 -0400

    push it again i guess..

---
+++ FrenchFrogsPhoenix/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Windows/Wwise_IDs.h
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID MENU = 2607556080U;
+        static const AkUniqueID SHIP_SPEED_BOOST = 1467867859U;
+        static const AkUniqueID SHIP_SPEED_BRAKE = 3082625079U;
+        static const AkUniqueID SHIP_SPEED_FORWARD = 3568150045U;
+        static const AkUniqueID SHIP_SPEED_IDLE = 3692956948U;
+        static const AkUniqueID START = 1281810935U;
+        static const AkUniqueID STICK = 1803197027U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SHIP_SPEED = 919421835U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID PRIVATEFROGSBANK = 1741871505U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
